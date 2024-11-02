60925b49715ebc3eb9d06f72cc849fb28a88ccb9
Raphael F <raphael.fontes@lais.huol.ufrn.br>
2024-11-02 10:29:10 -0300

    fix: icon github hover

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 w-full bg-gray-800 text-white flex justify-between items-center p-4 shadow-md">
+    <header className="fixed top-0 left-0 w-full text-gray-700 flex justify-between items-center p-4 shadow-md">
       <div className="text-xl font-bold">
         More-like-this
       </div>
@@ -12,7 +12,7 @@ const Header = () => {
         href="https://github.com" 
         target="_blank" 
         rel="noopener noreferrer" 
-        className="flex items-center space-x-2 bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded"
+        className="flex items-center space-x-2 hover:bg-gray-700 hover:text-white text-gray-700 font-bold py-2 px-4 rounded"
       >
         <FontAwesomeIcon icon={faGithub} className="w-6 h-6" />
       </a>
