dc14f8a06efedc014494da6b2eb51ddce6bfab51
Juciano <juciano.junior@hotmail.com>
2024-11-02 22:57:06 +0000

    Fix: position e estilo do header

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+import LeftMenu from "@/components/LeftMenu";
+
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="h-[120vh]">
+        <LeftMenu />
       </div>
     </>
   );
