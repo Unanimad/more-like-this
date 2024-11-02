fcc6d75f9d146522ee50917443034d2fc1c72769
Raphael F <raphael.fontes@lais.huol.ufrn.br>
2024-11-02 11:08:50 -0300

    fix footer

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-gray-600`}
       >
         <Header />
         {children}
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-gray-800 text-white py-4 mt-8">
+    <footer className="w-full bg-gray-50 py-4 mt-8">
       <div className="container mx-auto text-center">
         <span>&copy; {new Date().getFullYear()} Todos os direitos reservados.</span>
       </div>
