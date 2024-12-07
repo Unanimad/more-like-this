4aa84e6f53740458ba161c11c29ab6320ecb642c
Juciano <juciano.junior@hotmail.com>
2024-12-07 23:21:13 +0000

    Feat: iniciador do driver

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,13 @@
+"use client"
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { HiQuestionMarkCircle } from "react-icons/hi";
+import { driverInstance } from '@/app/driver/driver';
 
 const Header = () => {
   return (
-    <header className="sticky top-0 left-0 w-full bg-white text-gray-700 flex justify-between items-end p-4">
+    <header className="sticky top-0 left-0 w-full bg-white text-gray-700 flex justify-between items-center p-4">
       <a
         href="https://github.com"
         target="_blank"
@@ -13,6 +16,13 @@ const Header = () => {
       >
         <FontAwesomeIcon icon={faGithub} className="w-6 h-6" />
       </a>
+      <div className="align-middle">
+        <button onClick={() => driverInstance()}>
+          <div className='pt-2 hover:bg-gray-700 hover:text-white rounded'>
+            <HiQuestionMarkCircle size={30} />
+          </div>
+        </button>
+      </div>
     </header>
   );
 };
