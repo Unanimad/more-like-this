85b898a6ad2b151cbbace96f235d82ce2d1b5953
Juciano <juciano.junior@hotmail.com>
2024-12-07 23:19:37 +0000

    Refactor: concentrando hooks dentro do contexto de samples

--- context/SampleContext.js
+++ context/SampleContext.js
@@ -1,12 +1,68 @@
-import React, { createContext, useContext, useState } from 'react';
+import api from '@/services/instance';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
 const SampleContext = createContext();
 
 const SampleProvider = ({ children }) => {
+    const [configSimilarity, setSimilarityConfig] = useState({
+        // threshold: 0.5,
+        min_term_freq: 1,
+        max_query_terms: 25,
+        min_doc_freq: 1,
+        // max_doc_freq: 100,
+        min_word_length: 0,
+        // max_word_length: 0,
+        // stop_words: []
+    });
     const [samples, setSamples] = useState(null);
+    const [selectedIndex, setSelectedIndex] = useState(null);
+    const [selectedColumns, setSelectedColumns] = useState([]);
+    const [similarityResults, setSimilarityResults] = useState([]);
+
+    const handleSetConfigValue = (id, value) => {
+        setSimilarityConfig((prevConfig) => ({
+            ...prevConfig,
+            [id]: value,
+        }));
+    };
+
+    useEffect(() => {
+        const fetchSimilarityResults = async () => {
+            try {
+                const response = await api.post('/search_engine/more-like-this', {
+                    index: selectedIndex,
+                    fields: selectedColumns,
+                    doc_ids: samples,
+                    ...configSimilarity,
+                });
+
+                setSimilarityResults(response.data);
+            } catch (error) {
+                setSimilarityResults(null);
+                console.error('Error fetching similarity results:', error);
+            }
+        };
+
+        if (selectedIndex !== null && selectedColumns.length > 0 && samples !== null) {
+            fetchSimilarityResults();
+        }
+        console.log("FETCHING")
+    }, [selectedIndex, selectedColumns, samples, configSimilarity]);
 
     return (
-        <SampleContext.Provider value={{ samples, setSamples }}>
+        <SampleContext.Provider value={{
+            samples,
+            setSamples,
+            configSimilarity,
+            setSimilarityConfig,
+            selectedIndex,
+            setSelectedIndex,
+            selectedColumns,
+            setSelectedColumns,
+            similarityResults,
+            setSimilarityResults,
+            handleSetConfigValue
+        }}>
             {children}
         </SampleContext.Provider>
     );
